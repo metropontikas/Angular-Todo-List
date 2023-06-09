be85c636b73a8898a8e7df074286001fda1535ef
metropontikas <grafomaipantou@tutanota.com>
2023-06-10 00:05:17 +0300

    Fix missing declarations in Item Component

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,5 +1,11 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Item } from '../item';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.scss'],
+})
 export class ItemComponent {
   editable = false;
 
