c97b36ad73b34e5ad74a8dd25b8c5a98ed53c284
metropontikas <grafomaipantou@tutanota.com>
2023-06-11 22:45:04 +0300

    Add item in app.modules imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ItemComponent } from './item/item.component';
+import { ItemInputComponent } from './item-input/item-input.component';
 
 @NgModule({
-  declarations: [AppComponent, ItemComponent],
+  declarations: [AppComponent, ItemComponent, ItemInputComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
