c7497f1852388cd0e0f3df6ce3ec285c14161c2a
metropontikas <grafomaipantou@tutanota.com>
2023-06-11 22:55:51 +0300

    Initialize component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ItemComponent } from './item/item.component';
+import { InputFormComponent } from './input-form/input-form.component';
 
 @NgModule({
-  declarations: [AppComponent, ItemComponent],
+  declarations: [AppComponent, ItemComponent, InputFormComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/input-form/input-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-form',
+  templateUrl: './input-form.component.html',
+  styleUrls: ['./input-form.component.scss']
+})
+export class InputFormComponent {
+
+}
